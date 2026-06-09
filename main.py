f4517471c977b892d2492b188d808376dcc2df12
jvcr72 <jcardozo872@gmail.com>
2026-06-08 22:58:55 -0400

    Añadir ruta raíz para diagnóstico

--- main.py
+++ main.py
@@ -1,5 +1,10 @@
 from fastapi import FastAPI
+
 app = FastAPI()
+
+@app.get("/")
+def home():
+    return {"mensaje": "La API está funcionando en la raíz"}
 
 @app.get("/api/v1/salud")
 def check_salud():
