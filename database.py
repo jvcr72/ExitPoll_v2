f54438e4738884bc761d28cc26d604e2be22b49f
jvcr72 <jcardozo872@gmail.com>
2026-06-10 22:36:47 -0400

    Ajuste de formato para PostgreSQL

--- database.py
+++ database.py
@@ -1,14 +1,17 @@
+import os
 from sqlalchemy import create_engine
 from sqlalchemy.ext.declarative import declarative_base
 from sqlalchemy.orm import sessionmaker
-import os
 
-DATABASE_URL = os.environ.get("DATABASE_URL", "sqlite:///./votos.db")
+DATABASE_URL = os.environ.get("DATABASE_URL")
 
-if DATABASE_URL.startswith("postgres://"):
+# --- AGREGA ESTO ---
+# SQLAlchemy requiere que la URL empiece por "postgresql://", 
+# pero a veces el driver de Render viene como "postgres://".
+if DATABASE_URL and DATABASE_URL.startswith("postgres://"):
     DATABASE_URL = DATABASE_URL.replace("postgres://", "postgresql://", 1)
+# -------------------
 
-# Añadimos 'pool_pre_ping' para asegurar que la conexión esté viva
 engine = create_engine(DATABASE_URL, pool_pre_ping=True)
 SessionLocal = sessionmaker(autocommit=False, autoflush=False, bind=engine)
 Base = declarative_base()
