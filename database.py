45bf2332c6116bcadb7df697fe2d86ed3f756cbf
jvcr72 <jcardozo872@gmail.com>
2026-06-10 21:51:59 -0400

    Asegurar conexión con pool_pre_ping

--- database.py
+++ database.py
@@ -3,14 +3,12 @@ from sqlalchemy.ext.declarative import declarative_base
 from sqlalchemy.orm import sessionmaker
 import os
 
-# Render usa la variable DATABASE_URL que configuramos
-# Si no la encuentra (como cuando corres local), usa SQLite por defecto
 DATABASE_URL = os.environ.get("DATABASE_URL", "sqlite:///./votos.db")
 
-# Si la URL empieza por 'postgres://', SQLAlchemy necesita 'postgresql://'
 if DATABASE_URL.startswith("postgres://"):
     DATABASE_URL = DATABASE_URL.replace("postgres://", "postgresql://", 1)
 
-engine = create_engine(DATABASE_URL)
+# Añadimos 'pool_pre_ping' para asegurar que la conexión esté viva
+engine = create_engine(DATABASE_URL, pool_pre_ping=True)
 SessionLocal = sessionmaker(autocommit=False, autoflush=False, bind=engine)
 Base = declarative_base()
