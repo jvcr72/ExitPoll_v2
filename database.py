c707269617018374ac411185abab1e4593fc4544
jvcr72 <jcardozo872@gmail.com>
2026-06-14 20:58:30 -0400

    Entrega de main.py y database.py completos con ruta reset-admin

--- database.py
+++ database.py
@@ -5,9 +5,6 @@ import os
 
 SQLALCHEMY_DATABASE_URL = os.getenv("DATABASE_URL")
 
-# Al dejar el motor limpio, SQLAlchemy negociará la conexión 
-# basándose únicamente en lo que provee la DATABASE_URL
 engine = create_engine(SQLALCHEMY_DATABASE_URL)
-
 SessionLocal = sessionmaker(autocommit=False, autoflush=False, bind=engine)
 Base = declarative_base()
