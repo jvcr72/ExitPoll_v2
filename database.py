663bb332cec8638c977a59c4bdc64837ef516196
jvcr72 <jcardozo872@gmail.com>
2026-06-14 20:39:46 -0400

    Test de diagnóstico de conexión

--- database.py
+++ database.py
@@ -1,14 +1,9 @@
 from sqlalchemy import create_engine
-from sqlalchemy.ext.declarative import declarative_base
 from sqlalchemy.orm import sessionmaker
+from sqlalchemy.ext.declarative import declarative_base
 import os
 
-# Render añade el parámetro sslmode=require automáticamente a la cadena de conexión interna.
-# Si el error persiste, es posible que la URL esté mal copiada en Render.
 SQLALCHEMY_DATABASE_URL = os.getenv("DATABASE_URL")
-
-# Usamos una configuración mínima para evitar errores de tipo 'status 1'
-engine = create_engine(SQLALCHEMY_DATABASE_URL)
-
-SessionLocal = sessionmaker(autocommit=False, autoflush=False, bind=engine)
+engine = create_engine(SQLALCHEMY_DATABASE_URL, pool_pre_ping=True)
+SessionLocal = sessionmaker(bind=engine)
 Base = declarative_base()
