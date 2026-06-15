7d0bd9871cc1e7ed7cf47215f73343788584e4e2
jvcr72 <jcardozo872@gmail.com>
2026-06-14 21:09:01 -0400

    Estructura forzada para registro de ruta reset-admin

--- database.py
+++ database.py
@@ -4,7 +4,6 @@ from sqlalchemy.orm import sessionmaker
 import os
 
 SQLALCHEMY_DATABASE_URL = os.getenv("DATABASE_URL")
-
 engine = create_engine(SQLALCHEMY_DATABASE_URL)
 SessionLocal = sessionmaker(autocommit=False, autoflush=False, bind=engine)
 Base = declarative_base()
