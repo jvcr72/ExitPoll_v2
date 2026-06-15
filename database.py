c864afd5b17052340027b1521699eecbae8fc2e7
jvcr72 <jcardozo872@gmail.com>
2026-06-14 20:45:05 -0400

    Corrección final de parámetros de conexión SSL

--- database.py
+++ database.py
@@ -3,7 +3,13 @@ from sqlalchemy.orm import sessionmaker
 from sqlalchemy.ext.declarative import declarative_base
 import os
 
+# Eliminamos cualquier intento de sslmode=require
 SQLALCHEMY_DATABASE_URL = os.getenv("DATABASE_URL")
-engine = create_engine(SQLALCHEMY_DATABASE_URL, pool_pre_ping=True)
-SessionLocal = sessionmaker(bind=engine)
+
+# Al añadir ?sslmode=disable, le indicamos al driver de postgres que no fuerce el cifrado
+# Si tu URL ya tiene parámetros (?), usa &sslmode=disable
+conn_url = SQLALCHEMY_DATABASE_URL if "?" in SQLALCHEMY_DATABASE_URL else f"{SQLALCHEMY_DATABASE_URL}?sslmode=disable"
+
+engine = create_engine(conn_url)
+SessionLocal = sessionmaker(autocommit=False, autoflush=False, bind=engine)
 Base = declarative_base()
